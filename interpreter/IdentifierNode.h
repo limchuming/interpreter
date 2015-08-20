6d64d7f26ed98dad6397f0c765ff6d27807dd6a7
limchuming <limchuming@gmail.com>
2015-08-20 01:48:04 +0100

    Created AnyType class that allows for storing of any type

--- interpreter/IdentifierNode.h
+++ interpreter/IdentifierNode.h
@@ -1,35 +1,40 @@
 #ifndef IDENTIFIER
 #define IDENTIFIER
 
-#include <map>
-#include <string>
 #include <memory>
 
-class Identifier {
+class AnyType {
 public:
-	Identifier(const std::string &name_) {}
-	virtual ~Identifier() = 0;
+	void swap(AnyType& other) { other.data_ptr.swap(this->data_ptr); }
+
+	AnyType() :data_ptr(nullptr){}
+	template <typename T> AnyType(const T &data_) :data_ptr(new InputType<T>(data_)) {}
+	AnyType(const AnyType &other) :data_ptr(other.data_ptr->clone()) {}
+	AnyType &operator=(AnyType &other) {
+		AnyType(other).swap(*this);
+		return *this;
+	}
+	template <typename T>	T &get_data() { return (dynamic_cast<InputType<T> &>(*(data_ptr.get()))).data; }
+	template <typename T>	const T &get_data() const { return (dynamic_cast<InputType<T> &>(*(data_ptr.get()))).data; }
+
+private:
+
+	struct Base {
+		virtual ~Base() {}
+		virtual Base *clone() const = 0;
+	};
+
+	template <typename T>
+	struct InputType : Base {
+		InputType(const T &data_) :data(data_) {}
+		InputType(const InputType &newinput) :data(newinput.data) {}
+		Base *clone() const { return new InputType<T>(*this); }
+		T data;
+	};
+
+	std::unique_ptr<Base> data_ptr;
+
 };
 
-template<class T>
-class IdentifierTyped : public Identifier {
-public:
-	IdentifierTyped(const T &value_);
-	void set_value(const T&);
-	T get_value();
-private:
-	T value;
-};
-
-
-
-
-class IdentifierList {
-public:
-	auto get_identifier_value(const std::string &);
-private:
-	std::map<std::string, std::unique_ptr<Identifier>> identifier_map;
-}
 #endif
 
- 
--- interpreter/runner.cpp
+++ interpreter/runner.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include <string>
-
-
+#include "IdentifierNode.h"
 
 int main() {
+
+	AnyType test(4);
+	AnyType newobj;
+	newobj = test;
+	newobj.get_data<int>() = 99;
+	std::cout << test.get_data<int>() <<std::endl;
+	std::cout << newobj.get_data<int>() << std::endl;
+
 	
-		
-
-
-
 	int stop;
-	cin >> stop;
+	std::cin >> stop;
 
 	return 0;
 }
